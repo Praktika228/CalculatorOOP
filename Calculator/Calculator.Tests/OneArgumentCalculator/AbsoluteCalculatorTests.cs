f3d86526ac2bd3859420543266656cc06b7a8922
Praktika <koyashim.97@mail.ru>
2019-07-11 11:42:33 +0600

    finish first 10 tests

---
+++ Calculator/Calculator.Tests/OneArgumentCalculator/AbsoluteCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.OneArgumentCalculator;
+using NUnit.Framework;
+
+namespace Calculator.Tests.OneArgumentCalculator
+{
+    [TestFixture]
+    class AbsoluteCalculatorTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(-1, 1)]
+        public void CalculationTest(double firstValue, double expected)
+        {
+            var calculator = new AbsoluteCalculator();
+            var actualResult = calculator.Calculate(firstValue);
+            Assert.AreEqual(expected, actualResult, 0.1);
+        }
+    }
+}
---
+++ Calculator/Calculator.Tests/OneArgumentCalculator/OneDivOperandCalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using Calculator.OneArgumentCalculator;
+using NUnit.Framework;
+
+namespace Calculator.Tests.OneArgumentCalculator
+{
+    [TestFixture]
+    class OneDivOperandCalculatorTests
+    {
+        [TestCase(2, 0.5)]
+        [TestCase(1, 1)]
+        public void CalculationTest(double firstValue, double expected)
+        {
+            var calculator = new OneDivOperandCalculator();
+            var actualResult = calculator.Calculate(firstValue);
+            Assert.AreEqual(expected, actualResult, 0.1);
+        }
+    }
+}
