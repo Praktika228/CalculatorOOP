eb0fa390faecdab5c3a8a973a3ae80d6477d9cdf
Praktika <koyashim.97@mail.ru>
2019-07-11 12:40:59 +0600

    added test of fabric TwoArgumentsFactory

---
+++ Calculator/Calculator.Tests/TwoArgumentsCalculator/TwoArgumentsFactoryTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.TwoArgumentsCalculator;
+using NUnit.Framework;
+
+namespace Calculator.Tests.TwoArgumentsCalculator
+{
+    [TestFixture]
+    class TwoArgumentsFactoryTests
+    {
+        [TestCase("plus", typeof(AdditionCalculator))]
+        [TestCase("minus", typeof(SubstractionCalculator))]
+        [TestCase("division", typeof(DivisionCalculator))]
+        [TestCase("mod", typeof(ModCalculator))]
+        [TestCase("min", typeof(MinCalculator))]
+        [TestCase("max", typeof(MaxCalculator))]
+        [TestCase("logx", typeof(LogXY))]
+        [TestCase("average", typeof(AverageCalculator))]
+        [TestCase("geometric", typeof(GeometricCalculator))]
+        [TestCase("multiply", typeof(MultiplyCalculator))]
+
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = TwoArgumentsFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
